945b0093090f6df6b5acb86fcb64179d7bb99778
hamim <hamim2114@gmail.com>
2023-11-03 22:24:22 +0600

    section complate

--- src/pages/practiceAreasPage.jsx
+++ src/pages/practiceAreasPage.jsx
@@ -1,29 +1,36 @@
 import React from 'react'
-import { motion } from 'framer-motion';
 import { useQuery } from '@tanstack/react-query';
 import parser from 'html-react-parser';
 import './PracticeAreaPage.scss'
 import { axiosReq } from '../utils/axiosReq';
+import { Avatar, Box, Stack, Typography } from '@mui/material';
 
 const practiceAreasPage = () => {
   const { isLoading, error, data } = useQuery({
-    queryKey: ['practice'],
-    queryFn: () => axiosReq.get('/practice/').then(res => res.data)
+    queryKey: ['legal-services'],
+    queryFn: () => axiosReq.get('/legalServices/').then(res => res.data)
   });
 
   return (
     <div className="practice">
       <div className="wrapper">
+          <div className="h1">AREAS OF LEGAL SERVICES</div>
         {
           isLoading ? 'Loading..' : error ? 'Something went wrong!' :
-            <>
-              <div className="top">
-                <h1>{data[0]?.title}</h1>
-              </div>
-              <div className="bottom">
-                {parser(data[0].body)}
-              </div>
-            </>
+            <Stack direction={'row'} gap={6} flexWrap={'wrap'} justifyContent={'center'} mt={5}>
+              {
+                data.map((data, i) => (
+                  <Stack sx={{
+                    maxWidth: '300px',
+                    mb: 4
+                  }}>
+                    <Avatar sx={{alignSelf: 'center',mb: 2, width: '80px', height: '80px', opacity: '.7'}} variant="square" src={data.img} />
+                    <Typography variant='h5' sx={{fontSize: '30px', textAlign: 'center', mb: 2}}>{data.title}</Typography>
+                    <Typography sx={{color: 'gray'}}>{parser(data.body)}</Typography>
+                  </Stack>
+                ))
+              }
+            </Stack>
         }
       </div>
     </div>
